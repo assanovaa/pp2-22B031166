12f12ed6b4d128f24fdcbe76dab4fc4c62bc14be
assanovaa <122975189+assanovaa@users.noreply.github.com>
2023-01-26 00:45:46 +0600

    type

---
+++ tsis1/8.1.py
@@ -0,0 +1,30 @@
+x = "Hello World"
+a = 20
+b = 20.5
+y = 1j
+z = ["apple", "banana", "cherry"]
+m = ("apple", "banana", "cherry")
+n = range(6)
+o = {"apple", "banana", "cherry"}	
+r = frozenset({"apple", "banana", "cherry"})	
+p = True		
+h = b"Hello"		
+g = bytearray(5)		
+l = memoryview(bytes(5))	
+d = None
+
+print(type(x)) 
+print(type(a)) 
+print(type(b)) 
+print (type(y))
+print(type(z)) 
+print(type(m)) 
+print(type(n)) 
+print(type(o)) 
+print(type(r)) 
+print(type(p)) 
+print(type(h)) 
+print(type(g)) 
+print(type(l)) 
+print(type(d)) 
+
